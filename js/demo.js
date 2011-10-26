9b00b15b9c80b64036b241483cc2ac7a316bc1c6
Chris Coyier <chriscoyier@gmail.com>
2011-10-26 16:00:30 -0700

    IIFE to secure jQuery $ and closing previous popups before opening a new one

--- js/demo.js
+++ js/demo.js
@@ -1,35 +1,43 @@
-/* Faking Menu Functionality */
+(function($) {
 
-$(".main-nav li:has(ul)")
-	.addClass("has-submenu")
-	.hover(function() {
-		$(this).addClass("open");
-	}, function() {
-		$(this).removeClass("open");
-	});
+  /* Faking Menu Functionality */
 
-$(".main-nav > li > a ").click(function(e) {
-   e.preventDefault();
-   $(".main-nav > li").removeClass("active");
-   $(this).parent().addClass("active");
-});
+  $(".main-nav li:has(ul)")
+  	.addClass("has-submenu")
+  	.hover(function() {
+  		$(this).addClass("open");
+  	}, function() {
+  		$(this).removeClass("open");
+  	});
+
+  $(".main-nav > li > a ").click(function(e) {
+     e.preventDefault();
+     $(".main-nav > li").removeClass("active");
+     $(this).parent().addClass("active");
+  });
 
 
 
-/* Faking Help Popout Functionality */
+  /* Faking Help Popout Functionality */
+  
+  /* This is working like shit sandwich */
+  
+  var config = {    
+       over: openPopup,   
+       timeout: 750,    
+       out: closePopup  
+  },
+  allQs = $('.q');
 
-var config = {    
-     over: openPopup,   
-     timeout: 1000,    
-     out: closePopup  
-};
+  function openPopup(el) {
+    allQs.removeClass("open");
+    $(el.currentTarget).addClass("open");
+  };
 
-function openPopup(el) {
-  $(el.currentTarget).addClass("open");
-};
+  function closePopup(el) {
+    $(el.currentTarget).removeClass("open");
+  };
 
-function closePopup(el) {
-  $(el.currentTarget).removeClass("open");
-};
+  allQs.hoverIntent(config);
 
-$(".q").hoverIntent(config);
+})(jQuery);
