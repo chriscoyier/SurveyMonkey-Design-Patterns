b0724ee9dfd6dd5573171777ae0f889c62248625
Chris Coyier <chriscoyier@gmail.com>
2011-10-28 10:55:33 -0700

    newness

--- js/demo.js
+++ js/demo.js
@@ -2,13 +2,13 @@
 
   /* Faking Menu Functionality */
 
-  $(".main-nav li:has(ul)")
-  	.addClass("has-submenu")
-  	.hover(function() {
-  		$(this).addClass("open");
-  	}, function() {
-  		$(this).removeClass("open");
-  	});
+  // $(".main-nav li:has(ul)")
+  //  .addClass("has-submenu")
+  //  .hover(function() {
+  //    $(this).addClass("open");
+  //  }, function() {
+  //    $(this).removeClass("open");
+  //  });
 
   $(".main-nav > li > a ").click(function(e) {
      e.preventDefault();
