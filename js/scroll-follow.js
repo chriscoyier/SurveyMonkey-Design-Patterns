2477e2692d09b262b9d17973b5589d1ced421b1b
Chris Coyier <chriscoyier@gmail.com>
2011-11-26 15:20:17 -0800

    Better Scroll-Follow

--- js/scroll-follow.js
+++ js/scroll-follow.js
@@ -1,11 +1,33 @@
-
 (function($) {
-  
-  // Debounce?
-  $(window).scroll(function() {
-    
-    // console.log($("#motherfucker").is(":within-viewport-top"));
-    
-  });
-  
+
+	function updatePositions() {
+		
+			var parent    = $("#scroll-parent"),
+			    offset    = parent.offset(),
+			    scrollTop = $(window).scrollTop();
+
+			if ((scrollTop > offset.top) && (scrollTop < offset.top + parent.height())) {
+				follower.css({
+					"position": "fixed",
+					"top"     : 0,
+					"width"   : follower.data("orig-width")
+				})
+			} else {
+				follower.css({
+					"position": "static",
+					"width"   : "auto"
+				})
+			}
+	}
+	
+	var follower = $("#i-follow");
+	follower
+		.data("orig-width", follower.outerWidth())
+		.data("orig-left",  0);
+
+	$(window)
+	.scroll(updatePositions)
+	// Trigger once on load to kick it off
+	.trigger("scroll");
+
 })(jQuery);
